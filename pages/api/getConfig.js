6aaa781f5469f185431ef36d676874077983f398
Viktor Sarström <viktorsarstrom@gmail.com>
2021-09-10 19:26:28 +0200

    second try on cache

--- pages/api/getConfig.js
+++ pages/api/getConfig.js
@@ -4,6 +4,9 @@ import { findStrings, fantomenkrypto } from '../../lib/fantomenkrypto'
 export default function handler(req, res) {
 
    axios.get('https://etjanst.stockholm.se/vardnadshavare/bundles/childcontroller').then(({data}) => {
+
+    res.setHeader('Cache-Control', 's-maxage=600, stale-while-revalidate');
+
     const [_, stringz, numberz] = data.match(/"(.*)",(\d+)/)
     const str = stringz.replace(/\\\"/g, '"')
     const strings = fantomenkrypto(str,numberz);
