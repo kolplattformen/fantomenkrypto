2ebb6425791425358699818d651e9573597f4daa
Viktor Sarström <viktorsarstrom@gmail.com>
2021-09-10 17:04:36 +0200

    removed cache

--- pages/api/getConfig.js
+++ pages/api/getConfig.js
@@ -1,15 +1,12 @@
  const axios = require('axios');
 import { findStrings, fantomenkrypto } from '../../lib/fantomenkrypto'
 
-
 export default function handler(req, res) {
 
    axios.get('https://etjanst.stockholm.se/vardnadshavare/bundles/childcontroller').then(({data}) => {
     const [_, stringz, numberz] = data.match(/"(.*)",(\d+)/)
     const str = stringz.replace(/\\\"/g, '"')
     const strings = fantomenkrypto(str,numberz);
-
-    res.setHeader(`Cache-Control", "max-age=0, s-maxage=600, stale-while-revalidate`)
 
     res.status(200).json( { ...findStrings(strings), topologyBase64Iterations: 8 })
 
